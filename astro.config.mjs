6eaecfcb8f4a70b4d9ab89fc46742277dd25a202
sasithorn-k93 <sasithorn.klaysuwan@gmail.com>
2026-05-16 21:35:13 +0700

    css update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sasithorn-k93.github.io',
+  base: '/sasithorn-k',
   vite: {
     plugins: [tailwindcss()]
   }
